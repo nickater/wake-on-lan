7a89502ec15ca302ae68067e7df798d32b477e70
Nick Ater <nickolas.ater@gmail.com>
2020-09-21 16:57:39 -0400

    UPDATE

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const wol = require('wake_on_lan');
 const app = express();
 
 const port = 3001
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
+  wol.wake('98:48:27:40:0A:71');
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
