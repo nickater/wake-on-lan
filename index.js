254032962277c0676db45ad883f439d92bfd2ef4
Nick Ater <nickolas.ater@gmail.com>
2020-12-17 18:27:26 -0500

    added new wol endpoints

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ const app = express();
 const port = 3001
 
 app.get('/', (req, res) => {
+  fireUpNicksRig();
+  fireUpStaceysRig();
+})
+
+app.get('/pauerhaus', (req, res) => {
+  fireUpNicksRig();
+})
+
+app.get('/rss-rig', (req, res) => {
+  fireUpStaceysRig();
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+function fireUpNicksRig() {
   exec('wakeonlan 98:48:27:40:0A:71', (error, stdout) => {
     if(error instanceof Error) {
       res.send({
@@ -18,8 +35,20 @@ app.get('/', (req, res) => {
       });
     }
   })
-})
+}
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+function fireUpStaceysRig() {
+  exec('wakeonlan D8:07:B6:54:DF:32', (error, stdout) => {
+    if(error instanceof Error) {
+      res.send({
+        output: stderr,
+        msg: 'something went wrong'
+      });
+    } else {
+      res.send({
+        output: stdout,
+        msg: req.headers.msg || 'launch is successful'
+      });
+    }
+  })
+}
