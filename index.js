73c3ff673debade28658b41d78370191f8af85c5
Nick Ater <nickolas.ater@gmail.com>
2020-12-19 11:09:21 -0500

    reconfigured wake process

--- index.js
+++ index.js
@@ -5,27 +5,19 @@ const app = express();
 const port = 3001
 
 app.get('/', (req, res) => {
-  fireUpNicksRig();
-  fireUpStaceysRig();
+  const fireUpStacey = wakeOnLan('D8:07:B6:54:DF:32');
+  const fireUpNick = wakeOnLan('98:48:27:40:0A:71');
+  res.send({
+    pauerhaus: fireUpNick,
+    rssrig: fireUpStacey
+  });
 })
 
 app.get('/pauerhaus', (req, res) => {
-  fireUpNicksRig();
-})
-
-app.get('/rss-rig', (req, res) => {
-  fireUpStaceysRig();
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
-function fireUpNicksRig() {
   exec('wakeonlan 98:48:27:40:0A:71', (error, stdout) => {
     if(error instanceof Error) {
       res.send({
-        output: stderr,
+        output: stdout,
         msg: 'something went wrong'
       });
     } else {
@@ -35,13 +27,13 @@ function fireUpNicksRig() {
       });
     }
   })
-}
+})
 
-function fireUpStaceysRig() {
+app.get('/rss-rig', (req, res) => {
   exec('wakeonlan D8:07:B6:54:DF:32', (error, stdout) => {
     if(error instanceof Error) {
       res.send({
-        output: stderr,
+        output: stdout,
         msg: 'something went wrong'
       });
     } else {
@@ -49,6 +41,67 @@ function fireUpStaceysRig() {
         output: stdout,
         msg: req.headers.msg || 'launch is successful'
       });
+    }
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+// function fireUpNicksRig(res) {
+//   exec('wakeonlan 98:48:27:40:0A:71', (error, stdout) => {
+//     if(error instanceof Error) {
+//       res.send({
+//         output: stdout,
+//         msg: 'something went wrong'
+//       });
+//     } else {
+//       res.send({
+//         output: stdout,
+//         msg: req.headers.msg || 'launch is successful'
+//       });
+//     }
+//   })
+// }
+
+// function fireUpStaceysRig(res) {
+//   exec('wakeonlan D8:07:B6:54:DF:32', (error, stdout) => {
+//     if(error instanceof Error) {
+//       res.send({
+//         output: stdout,
+//         msg: 'something went wrong'
+//       });
+//     } else {
+//       res.send({
+//         output: stdout,
+//         msg: req.headers.msg || 'launch is successful'
+//       });
+//     }
+//   })
+// }
+
+// function fireUpBoth(res) {
+//   const fireUpStacey = wakeOnLan('D8:07:B6:54:DF:32');
+//   const fireUpNick = wakeOnLan('98:48:27:40:0A:71');
+//   res.send({
+//     pauerhaus: fireUpNick,
+//     rssrig: fireUpStacey
+//   });
+// }
+
+function wakeOnLan(macAddress) {
+  return exec(`wakeonlan ${macAddress}`, (error, stdout) => {
+    if(error instanceof Error) {
+      return {
+        output: {},
+        msg: 'something went wrong'
+      }
+    } else {
+     return {
+        output: {},
+        msg: req.headers.msg || 'launch is successful'
+      }
     }
   })
 }
