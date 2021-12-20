df2725b922b53cc849aa781839d933cc7b47564a
Nick Ater <nick@atercloud.com>
2021-12-19 22:38:47 -0500

    added wakeonlan package

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-const express = require('express');
-const exec = require('child_process').exec;
+import express from 'express';
+import wakeonlan from 'wakeonlan'
+
 const app = express();
 
-const port = 3001;
+const port = 3002;
 const NICK_MAC = '98:48:27:40:0A:71';
 const STACEY_MAC = 'D8:07:B6:54:DF:32';
 
-app.get('/pauerhaus', (req, res) => {
-  exec(`wakeonlan ${NICK_MAC}`, (stderr, stdout) => {
-    res.send({ msg: 'OK' })
-  });
-
+app.get('/pauerhaus', async (req, res) => {
+  const result = await wakeonlan(NICK_MAC);
+  res.send({ msg: 'OK' })
 })
 
-app.get('/rss-rig', (req, res) => {
-  exec(`wakeonlan ${STACEY_MAC}`, (stderr, stdout) => {
-    res.send({ msg: 'OK' })
-  });
-
+app.get('/rss-rig', async (req, res) => {
+  const result = await wakeonlan(STACEY_MAC);
+  res.send({ msg: 'OK' })
 })
 
 app.listen(port, () => {
