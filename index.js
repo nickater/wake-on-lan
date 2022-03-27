f626468ec92142ce80f727648ffae0f4dcb9745d
Nick Ater <nick@atercloud.com>
2022-03-27 15:01:49 -0400

    made app more loosely coupled

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 import express from 'express';
-import wakeonlan from 'wakeonlan'
-
+import { send } from './src/wake-on-lan.js'
 const app = express();
 
-const port = 3001;
-const NICK_MAC = '98:48:27:40:0A:71';
-const STACEY_MAC = 'D8:07:B6:54:DF:32';
+const port = process.env.WOL_PORT || 3001;
+const envVars = process.env
+const macAddresses = [];
 
-app.get('/pauerhaus', async (req, res) => {
-  const result = await wakeonlan(NICK_MAC);
-  res.send({ msg: 'OK' })
+Object.keys(envVars).forEach(variable => {
+  if (variable.startsWith('WOL_') && !variable.includes('PORT')) {
+    let cleanVar = variable.replace('WOL_', '').toLowerCase();
+    macAddresses.push({ key: cleanVar, value: envVars[variable] })
+  }
 })
 
-app.get('/rss-rig', async (req, res) => {
-  const result = await wakeonlan(STACEY_MAC);
-  res.send({ msg: 'OK' })
+if (macAddresses.length === 0) {
+  throw new Error('No MAC address environment variables set with a key that start with WOL_')
+} else {
+  console.log('Your WOL endpoints have been setup. They are:')
+  macAddresses.forEach(address => console.log(`/${address.key}`))
+}
+
+macAddresses.forEach(endpoint => {
+  app.get(`/${endpoint.key}`, async (req, res) => {
+    try {
+      const result = await send(endpoint.value);
+      res.send({ msg: 'OK', result })
+    } catch (error) {
+      res.json(error).send()
+    }
+  })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`WOL app listening at http://localhost:${port}`)
 })
 
